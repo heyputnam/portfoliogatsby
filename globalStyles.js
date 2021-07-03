e71dca109f4c1e415942b6a3fc406f5484de37f5
Hailey <hailey@macbook-pro.myfiosgateway.com>
2021-07-03 15:53:50 -0400

    added full page for sections

---
+++ globalStyles.js
@@ -0,0 +1,68 @@
+// creating global styles
+import * as React from 'react'
+
+const { createGlobalStyle } = require("styled-components");
+
+
+export const GlobalStyle = createGlobalStyle`
+:root{
+    --white: #ffffff;
+    --black: #000000;
+}
+*,*::before,*::after{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'PT Sans Caption', sans-serif;
+}
+html{
+    scroll-behavior: smooth;
+}
+body, html, a{
+    font-family:  'PT Sans Caption', sans-serif;
+}
+body{
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    background: var(--black);
+    overflow-x: hidden;
+}
+h1, h2, h3, h4, h5, h6{
+    margin: 0;
+    padding: 0;
+}
+a{
+    text-decoration: none;
+    outline: none;
+}
+button{
+    border: none;
+    outline: none;
+    &:focus{
+        outline: none;
+    }
+}
+*:focus{
+    outline: none;
+}
+img{
+    width: 100%;
+    height: auto; 
+}
+`
+
+
+const Layout = ({children}) =>{
+    return(
+        <>
+        <GlobalStyle/>
+    {children}
+    </>
+    )
+
+
+}
+
+export default Layout; 
