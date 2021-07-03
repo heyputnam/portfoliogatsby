96e5628ab03d4e9535700314af2e6cca3f0d27bf
Hailey <hailey@macbook-pro.myfiosgateway.com>
2021-07-03 15:32:35 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-sitemap",
+  ],
+};
