414f4da30e43f233492683da88cb99a1945288af
Hailey <hailey@MacBook-Pro.local>
2021-07-05 20:28:39 -0400

    ready to add data

--- components/projects.js
+++ components/projects.js
@@ -1,48 +1,24 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import kayotime from '../src/images/kayotime.png'
-
+import Tile from './tile'
 
 const Project =styled.div`
-
 display: grid;
-grid-template-columns: 3;
-grid-template-rows: 2;
+grid-template-columns: 33% 33% 33%;
 `
-const Tile =styled.div`
-height: 12em;
-width: 400px;
-cursor: pointer;
-margin: 1rem;
-&:hover{
-    img{
-        filter: blur(2px);
-    }
-    h1{
-        filter: blur(0);
-        display: block;
-    }
-}
-h1{
-    z-index: 1;
-    text-align: center;
-    margin-top: -8rem;
-    display: none;
-}
 
-`
-const Projects = () => {
+
+const Projects = ({link, image, title}) => {
     return (
         <>
             <Project>
-                <Tile>
-                    <a href="/kayotime"><img src={kayotime}></img></a>
-        <h1>Kayo Time Details</h1>
-                </Tile>
-                <Tile>
-                    <img src={kayotime}></img>
-        <h1>Kayo Time Details</h1>
-                </Tile>
+  <Tile
+  title="Kayo Time Details"
+  link="/kayotime"
+  img={kayotime}/>
+  <Tile/>
+
             </Project>
         </>
     )
